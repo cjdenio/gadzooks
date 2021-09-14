b259052de4aa72b6e5b0ad271a358b92509ec62a
Caleb Denio <cjdenio44@gmail.com>
2021-09-14 01:00:16 -0400

    responsiveness but like not really

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import logo from "./logo.svg";
 function App() {
   return (
     <div>
-      <div className="text-center bg-white w-3/5 fixed left-0 right-0 mx-auto top-3 py-4 px-20 rounded-full shadow-lg max-w-5xl flex items-center justify-between">
+      <div className="header text-center bg-white w-4/5 fixed left-0 right-0 mx-auto top-3 py-4 px-20 rounded-full shadow-lg max-w-5xl flex items-center justify-between">
         <button>Home</button>
         <button>Our Services</button>
 
@@ -22,25 +22,27 @@ function App() {
       </div>
 
       <div className="bg-purple-500 flex flex-col justify-center items-center h-screen text-white">
-        <h1 className="text-6xl mb-4 font-bold">
-          You need a{" "}
-          <ReactRotatingText
-            items={[
-              "wedding cake",
-              "shelf",
-              "robot",
-              "rare fish",
-              "fantastical forest",
-              "website",
-            ]}
-          />
-          .
-        </h1>
-        <h2 className="text-3xl">Gadzooks makes it easy as 🥧.</h2>
+        <div className="w-4/5 text-center">
+          <h1 className="text-6xl mb-4 font-bold">
+            You need a{" "}
+            <ReactRotatingText
+              items={[
+                "wedding cake",
+                "shelf",
+                "robot",
+                "rare fish",
+                "fantastical forest",
+                "website",
+              ]}
+            />
+            .
+          </h1>
+          <h2 className="text-3xl">Gadzooks makes it easy as 🥧.</h2>
+        </div>
       </div>
 
       <div className="py-20">
-        <div className="w-full max-w-3xl mx-auto">
+        <div className="w-4/5 max-w-3xl mx-auto">
           <h1 className="text-6xl mb-4 font-bold">
             Handling your idea the whole way through.
           </h1>
@@ -49,8 +51,8 @@ function App() {
             claim).
           </h2>
 
-          <div className="flex">
-            <div className="shadow-md flex-1 mr-5 bg-purple-500 text-white p-5 rounded-lg">
+          <div className="flex gap-3 flex-wrap">
+            <div className="shadow-md flex-1 bg-purple-500 text-white p-5 rounded-lg">
               <h2 className="text-4xl font-bold mb-3">
                 It all begins with a <em>spark</em>.
               </h2>
@@ -58,7 +60,7 @@ function App() {
               forest along the way.
             </div>
 
-            <div className="shadow-md flex-1 mr-5 bg-purple-500 text-white p-5 rounded-lg">
+            <div className="shadow-md flex-1 bg-purple-500 text-white p-5 rounded-lg">
               <h2 className="text-4xl font-bold mb-3">The end is in sight.</h2>
               Is this the light at the end of the tunnel, or is this the light
               of a fast-approaching train? 🤔🚊
@@ -68,14 +70,14 @@ function App() {
       </div>
 
       <div className="py-20 bg-green-400">
-        <div className="w-full max-w-3xl mx-auto">
+        <div className="w-4/5 max-w-3xl mx-auto">
           <h1 className="text-6xl mb-4 font-bold">End-to-end verification.</h1>
           <h2 className="mb-10">
             Robots are dangerous. Your life doesn't have to be.
           </h2>
 
-          <div className="flex">
-            <div className="shadow-md flex-1 mr-5 bg-white p-5 rounded-lg">
+          <div className="flex gap-3 flex-wrap">
+            <div className="shadow-md flex-1 bg-white p-5 rounded-lg">
               <h2 className="text-4xl font-bold mb-3">
                 Captchas from start to finish.
               </h2>
@@ -84,7 +86,7 @@ function App() {
               sure hope not.
             </div>
 
-            <div className="shadow-md flex-1 mr-5 bg-white p-5 rounded-lg">
+            <div className="shadow-md flex-1 bg-white p-5 rounded-lg">
               <h2 className="text-4xl font-bold mb-3">
                 <span className="font-light">Something for everyone.</span>{" "}
                 Robots too.
@@ -98,7 +100,7 @@ function App() {
       </div>
 
       <div className="py-20">
-        <div className="w-full max-w-3xl mx-auto">
+        <div className="w-4/5 max-w-3xl mx-auto">
           <h1 className="text-6xl mb-4 font-bold">
             You deserve best-in-class support.
           </h1>
@@ -107,15 +109,15 @@ function App() {
             IRC, and first-class mail (graciously provided by Corgi Logistics).
           </h2>
 
-          <div className="flex">
-            <div className="shadow-md flex-1 mr-5 bg-green-400 p-5 rounded-lg">
+          <div className="flex gap-3 flex-wrap">
+            <div className="shadow-md flex-1 bg-green-400 p-5 rounded-lg">
               <h2 className="text-4xl font-bold mb-3">Yes, yes, and yes.</h2>
               No complicated pricing plans, just the very best for you, your
               family, your dog, that rat that's been hiding in your ceiling
               since 2007, your broom, and everyone else.
             </div>
 
-            <div className="shadow-md flex-1 mr-5 bg-purple-500 text-white p-5 rounded-lg">
+            <div className="shadow-md flex-1 bg-purple-500 text-white p-5 rounded-lg">
               <h2 className="text-4xl font-bold mb-3">Better watch out.</h2>
               Santa's on his way in a sleigh, and he understands the impact
               Gadzooks can have on your business or fellowship. Reindeer are
@@ -126,7 +128,7 @@ function App() {
       </div>
 
       <div className="py-20 bg-purple-500 text-white">
-        <div className="w-full max-w-3xl mx-auto">
+        <div className="w-4/5 max-w-3xl mx-auto">
           <h1 className="text-6xl mb-4 font-bold">Can you hear me now?</h1>
           <h2 className="mb-10">
             Is my mic on? Yes? Good. Try a risk-free trial, no credit card (or

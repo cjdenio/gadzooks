d6450920bef14c43df751e8f3d2d3acbc65cc9ce
Caleb Denio <cjdenio44@gmail.com>
2021-09-14 16:51:29 -0400

    about page n stuff

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,14 +1,35 @@
 import React from "react";
 import Header from "../components/Header";
+import caleb from "../img/caleb.jpg";
 
 export default function AboutPage() {
   return (
     <div>
       <Header />
-      <div className="bg-purple-500 text-center pb-20 pt-40 text-white mb-20">
-        <h1 className="text-6xl font-bold">A man with a plan.</h1>
+      <div className="bg-purple-500 text-center pb-20 pt-36 text-white mb-20">
+        <div className="w-4/5 text-center mx-auto">
+          <h1 className="text-6xl font-bold">About Us</h1>
+        </div>
       </div>
-      to be continued...
+
+      <div className="w-4/5 max-w-3xl mx-auto py-5">
+        <h1 className="text-6xl font-bold mb-10">A man with a plan.</h1>
+
+        <div className="flex gap-5 flex-wrap-reverse items-center">
+          <div className="flex-1 border-purple-500 border-l-4 border-solid pl-4 min-w-200">
+            As <b className="text-purple-500">Caleb Denio</b> gazed over the
+            South Dakotan plain, he realized this industry was <b>broken</b>.
+            Resolving to fix it, he founded{" "}
+            <b className="text-purple-500 font-logo">Gadzooks</b>, a new kind of
+            company dedicated to restoring a way of life.
+          </div>
+          <img
+            src={caleb}
+            alt="Caleb Denio"
+            className="flex-1 w-full rounded-lg shadow-md min-w-200"
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,6 +21,10 @@ export default function IndexPage() {
                 "fantastical forest",
                 "website",
                 "friend",
+                "pet",
+                "life",
+                "pastime",
+                "job",
               ]}
             />
             .

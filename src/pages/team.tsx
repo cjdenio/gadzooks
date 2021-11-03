969a6372c7edd17de064de55c7bf8f4b031563d3
Caleb Denio <cjdenio44@gmail.com>
2021-11-03 15:32:17 -0400

    general council

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -66,8 +66,8 @@ export default function TeamPage() {
           />
           <div className="flex-1 border-purple-500 border-r-4 border-solid pr-4 min-w-200">
             <b className="text-purple-500">Deven</b> joined{" "}
-            <b className="text-purple-500 font-logo">Gadzooks</b> as a managing
-            partner in 1988, and has been a crucial part of our team ever since.
+            <b className="text-purple-500 font-logo">Gadzooks</b> as part of our
+            general council, and has been a crucial part of our team ever since.
             He oversees the large-scale production of beverages, children's
             toys, and animal collars.
           </div>
